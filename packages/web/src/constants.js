90a3fcf191b7831f83527c0f349bda79ea521bfb
RedJanvier <janvierntwali@gmail.com>
2021-07-21 06:07:07 +0200

    fix(client): api link to client

--- packages/web/src/constants.js
+++ packages/web/src/constants.js
@@ -1,7 +1,7 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:4000"
-    : "https://newsposter.herokuapp.com";
+    : process.env.REACT_APP_BASE_URL;
 
 export const NODE_ENV = process.env.NODE_ENV;
 export const ENDPOINTS = {
