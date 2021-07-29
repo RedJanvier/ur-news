3825fc587544608663bd1553f787da083877063d
RedJanvier <janvierntwali@gmail.com>
2021-07-29 02:57:37 +0200

    feat(profile): support public profile by regnumbers
    - find any person's profile with registration number

--- packages/web/src/components/ChangeProfilePic/ChangeProfilePic.js
+++ packages/web/src/components/ChangeProfilePic/ChangeProfilePic.js
@@ -1,35 +1,36 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useHistory } from "react-router-dom";
-import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory, useParams } from 'react-router-dom';
+import { faPencilAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import "./ChangeProfile.css";
-import Modal from "../Modal/Modal";
-import { BASE_URL } from "../../constants";
-import Spinner from "../Spinner/Spinner";
-import { GlobalContext } from "../../context/GlobalState";
+import './ChangeProfile.css';
+import Modal from '../Modal/Modal';
+import { BASE_URL } from '../../constants';
+import Spinner from '../Spinner/Spinner';
+import { GlobalContext } from '../../context/GlobalState';
 
 const ChangeProfile = () => {
-  const [img, setImg] = useState("");
+  const [img, setImg] = useState('');
+  const { regNumber } = useParams();
   const [imgUrl, setImgUrl] = useState(
-    "https://res.cloudinary.com/redjanvier/image/upload/v1592564639/blank-profile-picture-973460_640_l3acum.png"
+    'https://res.cloudinary.com/redjanvier/image/upload/v1592564639/blank-profile-picture-973460_640_l3acum.png'
   );
   const editBtn = <FontAwesomeIcon icon={faPencilAlt} />;
-  const { userId, token } = useContext(GlobalContext);
   const [loading, setLoading] = useState(false);
+  const { token } = useContext(GlobalContext);
   const history = useHistory();
 
   useEffect(() => {
     (async () => {
-      const res = await fetch(`${BASE_URL}/api/v1/users/${userId}`);
-      const {
-        success,
-        data: { image },
-      } = await res.json();
+      const res = await fetch(`${BASE_URL}/api/v1/users/${regNumber}`);
+      const { success, data } = await res.json();
+      console.log(data);
 
-      success && setImgUrl(image);
+      if (success) {
+        setImgUrl(data.image);
+      }
     })();
-  }, [userId]);
+  }, []);
 
   const handleFileSelect = (e) => {
     let reader = new FileReader();
@@ -49,7 +50,7 @@ const ChangeProfile = () => {
     setLoading(true);
 
     const formData = new FormData();
-    formData.append("img", img);
+    formData.append('img', img);
 
     try {
       const config = {
@@ -57,12 +58,12 @@ const ChangeProfile = () => {
           Authorization: `Bearer ${token}`,
         },
         body: formData,
-        method: "POST",
+        method: 'POST',
       };
 
       const res = await fetch(`${BASE_URL}/api/v1/users/profile`, config);
       const response = await res.json();
-      if (response.success) history.push("/");
+      if (response.success) history.push('/');
       setLoading(false);
     } catch (error) {
       console.log(error);
@@ -70,25 +71,25 @@ const ChangeProfile = () => {
   };
 
   return (
-    <div className="profile">
+    <div className='profile'>
       {imgUrl && (
-        <div className="profile-preview">
-          <img src={imgUrl} alt="current-profile" />
+        <div className='profile-preview'>
+          <img src={imgUrl} alt='current-profile' />
         </div>
       )}
-      <div className="edit-btn">{editBtn}</div>
-      <Modal title="Update Profile Picture">
+      <div className='edit-btn'>{editBtn}</div>
+      <Modal title='Update Profile Picture'>
         <form onSubmit={handleSubmit}>
-          <div className="form-control">
+          <div className='form-control'>
             <input
-              type="file"
-              accept="image/jpeg"
+              type='file'
+              accept='image/jpeg'
               onChange={handleFileSelect}
             />
           </div>
-          <div className="form-control">
-            <button type="submit" className="btn">
-              {!loading ? "Update" : <Spinner />}
+          <div className='form-control'>
+            <button type='submit' className='btn'>
+              {!loading ? 'Update' : <Spinner />}
             </button>
           </div>
         </form>
