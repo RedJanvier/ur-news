37030b22120e0ea55432e9eaff7d42eedd763ca6
redjanvier <janvierntwali@gmail.com>
2021-09-20 00:23:26 +0200

    chore(typescript): server and web support
    BREAKING CHANGE: Typescript 4.x version support for both web and server

--- packages/server/src/utils/errorRespones.ts
+++ packages/server/src/utils/errorRespones.ts
@@ -1,7 +1,6 @@
 class ErrorResponse extends Error {
-  constructor(message = 'Server Error!', statusCode = 500) {
+  constructor(public message = 'Server Error!', public statusCode = 500) {
     super(message);
-    this.statusCode = statusCode;
   }
 }
 
